76a40880467e51d8124178e1b280d678d3ab7808
li9633 <1367429633@qq.com>
2025-03-07 00:48:24 +0800

    feat/need fix can't find controlsfx

--- src/main/java/com/Auto7zip/services/FileServices.java
+++ src/main/java/com/Auto7zip/services/FileServices.java
@@ -1,15 +1,10 @@
 package com.Auto7zip.services;
 
 import java.io.File;
-import java.util.Optional;
-
 import com.Auto7zip.config.content.ChooserContent;
 
-import javafx.scene.control.ChoiceDialog;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
 import javafx.stage.Window;
-import org.controlsfx.control.FileChooser;
 
 public class FileServices {
     private FileServices() {
@@ -17,22 +12,13 @@ public class FileServices {
     }
 
     public static void selectionFolder(Window MainWindow) {
-        // DirectoryChooser dirChooser = new DirectoryChooser();
-        // dirChooser.setTitle(ChooserContent.getChooserText());
-        // FileChooser fileChooser = new FileChooser();
-        // fileChooser.setTitle(ChooserContent.getChooserText());
+        DirectoryChooser dirChooser = new DirectoryChooser();
+        dirChooser.setTitle(ChooserContent.getChooserText());
 
-        // File selectedFile = fileChooser.showOpenDialog(MainWindow);
-        // File selectedDirectory = dirChooser.showDialog(MainWindow);
-        // if (selectedDirectory != null) {
-        // System.out.println(selectedDirectory.getAbsolutePath());
-        // }
-        // if (selectedFile != null) {
-        // System.out.println(selectedFile.getAbsolutePath());
-        // }
-        org.controlsfx.control.FileChooser chooser = new org.controlsfx.control.FileChooser();
-        chooser.setMode(FileChooserMode.ANY); // 允许选择文件或目录
-        File file = chooser.showOpenDialog(stage);
+        File selectedDirectory = dirChooser.showDialog(MainWindow);
+        if (selectedDirectory != null) {
+        System.out.println(selectedDirectory.getAbsolutePath());
+        }
 
     }
 }
